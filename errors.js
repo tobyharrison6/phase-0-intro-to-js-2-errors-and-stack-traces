05eb302a80388f7071d773a0f487f7e8c0e82d15
tobyharrison6 <tobyharrison6@gmail.com>
2022-09-18 19:54:11 +1000

    Done with assignment

--- errors.js
+++ errors.js
@@ -2,19 +2,23 @@
 
 const snackSelection = 'Pretzels';
 
-if (snackSelection = 'Pretzels') {
+if (snackSelection === 'Pretzels') {
   console.log("That'll be $1, please!");
 }
 
 
 // Once the code above this line is fixed you'll be able to see the error from the code below
 
-function first () {
+function first() {
   second();
 }
 
-function second () {
+function second() {
   third();
+}
+
+function third() {
+  console.log("Now I'm a function!");
 }
 
 first();
